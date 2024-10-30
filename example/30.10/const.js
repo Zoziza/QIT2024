4f9eb5d65ed5016183c14840b8018deb593de037
Aziza <azizanurzan@gmail.com>
2024-10-30 12:39:55 +0600

    zadanie

---
+++ example/30.10/const.js
@@ -0,0 +1,8 @@
+var name = "Aty"; //Ozgeretin man
+let surname = "ABC";// Ozgeredi
+surname = "ABC";//surname alinamaly mani ozgeredi
+const id ="909090";//Tyragti man 
+id -"5675757";//Qate qaitarady
+
+console.log(id);//Syqpaidy
+
