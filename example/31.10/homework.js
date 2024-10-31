198c216b08d8c454060930d55f31bbfe2b60deac
Aziza <azizanurzan@gmail.com>
2024-10-31 18:42:07 +0600

    homework 31.10

---
+++ example/31.10/homework.js
@@ -0,0 +1,20 @@
+let slicesPerPizza = 8;
+let numberOfPizza = 5;
+ 
+let totalSlices = slicesPerPizza * numberOfPizza;
+
+console.log(totalSlices);// Жауабы  40 
+
+//  Тапсырма 2
+let totalChocolates = 24;
+let numberOfFriends = 6;
+
+let chocolatesPerFriend = totalChocolates / numberOfFriends;
+
+console.log(chocolatesPerFriend );// Жауабы  4
+
+//  Тапсырма 3 
+let plantHeight = 10;
+plantHeight += 2;
+
+console.log(plantHeight);// Жауабы 12
