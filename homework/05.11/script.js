86f5612e6d1d23e00b6126509bcba6c1a56afc61
Aziza <azizanurzan@gmail.com>
2024-11-05 18:48:33 +0600

    05.11 homework

---
+++ homework/05.11/script.js
@@ -0,0 +1,20 @@
+// 1 zadanue
+/*var use = Number(prompt("Жасынызды жазыныз: "));
+var age = 18;
+if ( age <= use ) {
+    alert ("Сіз " + age + "  толғансыз");
+} else {
+     alert ("Сіз " + age + " толмағансыз" );
+}*/
+
+// 2 zadanue 
+ var use = Number(prompt("Далада қанша градусты енгізіңіз :"));
+ var temperature = - 0; 
+
+ 
+ if (temperature >= use) {
+    alert ("Жылы шалбар" + "\n" + "Қолғаптар" )
+ } else {
+    alert ("Жеңіл шорты, футболка кисе жеткілікті");
+ }
+   
