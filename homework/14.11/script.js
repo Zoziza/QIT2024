03a31b94433cb35d56f82e268e93d7dae21445ad
Aziza <azizanurzan@gmail.com>
2024-11-26 14:30:07 +0600

    hm 15.11

---
+++ homework/14.11/script.js
@@ -0,0 +1,17 @@
+let correctPass = "amjilt_the_best"
+let attemps = 0;
+
+do {
+    let use = prompt("Введите правильный пороль...");
+
+  if  (use == correctPass) {
+    alert("Поздравляем , вы зашлит в сайт");
+    break;
+  } else {
+    attemps++;
+    if (attemps >= 5 )
+        alert ("Too many attempts");
+  } 
+} while(attemps < 5 )
+
+л
